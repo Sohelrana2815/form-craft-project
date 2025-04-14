fbeb80a7427935b0d34d9276b8fd3ea3b4cff2b1
Sohelrana2815 <sohel152302@gmail.com>
2025-04-14 07:10:31 +0600

    done

--- form-craft-backend/controllers/authController.js
+++ form-craft-backend/controllers/authController.js
@@ -1,24 +1,59 @@
 const jwt = require("jsonwebtoken");
 const db = require("../db");
+const admin = require("firebase-admin");
 
 exports.signupUser = async (req, res) => {
   const { name, email, uid } = req.body;
   console.log(name, email, uid);
 
   try {
+    // 1. Insert into database first
     const result = await db.query(
       "INSERT INTO users (name, email, uid) VALUES ($1, $2, $3) RETURNING *",
       [name, email, uid]
     );
     res.status(201).json(result.rows[0]);
   } catch (error) {
-    if (error.code === 23505) {
-      // Email unique error code
-      return res.status(409).json({ error: "Email already in use." });
+    if (error.code === "23505") {
+      try {
+        await admin.auth().deleteUser(uid);
+      } catch (firebaseError) {
+        console.error("Firebase Error", firebaseError);
+      }
     } else {
-      console.log("Error inserting users data.", error);
-      res.status(500).json({ error: "Internal server error." });
+      res.status(500).json({ error: "Internal server error" });
     }
+  }
+};
+
+exports.checkConflict = async (req, res) => {
+  const { name, email } = req.body;
+  console.log(name, email);
+
+  try {
+    // Check for existing email
+    const emailCheck = await db.query(
+      "SELECT email FROM users WHERE email = $1",
+      [email]
+    );
+
+    // Check for existing name
+
+    const nameCheck = await db.query("SELECT name FROM users WHERE name = $1", [
+      name,
+    ]);
+
+    if (emailCheck.rows.length > 0) {
+      return res.status(409).json({ error: "Email already exists!" });
+    }
+
+    if (nameCheck.rows.length > 0) {
+      return res.status(409).json({ error: "Name already taken!" });
+    }
+    // No conflict found
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
--- form-craft-backend/routes/authRoutes.js
+++ form-craft-backend/routes/authRoutes.js
@@ -5,5 +5,5 @@ const authController = require("../controllers/authController");
 // Authentication-related routes
 router.post("/signup", authController.signupUser);
 router.patch("/login/:email", authController.updateLogin);
-
+router.post("/check-user", authController.checkConflict);
 module.exports = router;

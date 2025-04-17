0b78c45fa7fa4d5f15d471d57bdd051ac79bea3c
Sohelrana2815 <sohel152302@gmail.com>
2025-04-17 08:41:20 +0600

    done

--- form-craft-frontend/src/components/ImageUploader.jsx
+++ form-craft-frontend/src/components/ImageUploader.jsx
@@ -1,10 +1,32 @@
-
+import { Box, Button } from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 const ImageUploader = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Box className="my-4">
+      {/* File input */}
+      <input
+        type="file"
+        accept="image/*"
+        id="image-upload"
+        style={{ display: "none" }}
+      />
+      {/* Custom button  */}
+      <label htmlFor="image-upload">
+        <Button
+          variant="outlined"
+          component="span"
+          startIcon={<CloudUploadIcon />}
+        >
+          Upload image
+        </Button>
+      </label>
+
+      {/* Preview section */}
+      <Box className="border p-8 rounded-md text-center my-4">
+        <p className="text-gray-500">Image show here.</p>
+      </Box>
+    </Box>
+  );
 };
 
 export default ImageUploader;
--- form-craft-frontend/src/pages/createTemplate/CreateTemplate.jsx
+++ form-craft-frontend/src/pages/createTemplate/CreateTemplate.jsx
@@ -1,6 +1,7 @@
 import { FormProvider, useForm } from "react-hook-form";
 import TitleDescriptionSection from "../../components/TitleDescriptionSection";
 import { Button } from "@mui/material";
+import ImageUploader from "../../components/ImageUploader";
 
 const CreateTemplate = () => {
   //   const { handleSubmit, control } = useForm();
@@ -12,8 +13,9 @@ const CreateTemplate = () => {
   };
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} className="p-4">
         <TitleDescriptionSection />
+        <ImageUploader />
         <Button type="submit" variant="contained">
           Save
         </Button>

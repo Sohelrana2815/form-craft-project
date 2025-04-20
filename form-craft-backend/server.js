3729a52f547fd1ce98c5bd6e6d5e6ef5d7b2db8a
Sohelrana2815 <sohel152302@gmail.com>
2025-04-20 07:15:13 +0600

    done

--- form-craft-backend/server.js
+++ form-craft-backend/server.js
@@ -32,5 +32,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Form craft running...${PORT}`);
 });

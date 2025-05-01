4376100cf748c4b14e69f661dca3f1b357db247a
Sohelrana2815 <sohel152302@gmail.com>
2025-05-01 07:44:31 +0600

    dark mode styling

--- form-craft-frontend/src/hooks/useAxiosPublic.jsx
+++ form-craft-frontend/src/hooks/useAxiosPublic.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://form-craft-backend.onrender.com/api",
 });
 const useAxiosPublic = () => {
   return axiosPublic;

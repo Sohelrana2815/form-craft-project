2b64fd596d175e32e5567e23a7fee95c71d55c18
Sohelrana2815 <sohel152302@gmail.com>
2025-05-01 10:44:46 +0600

    fix my db.js file

--- form-craft-backend/db.js
+++ form-craft-backend/db.js
@@ -1,4 +1,21 @@
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
-module.exports = prisma;
+
+const testDatabase = async () => {
+  try {
+    // Asking one raw & one column from database
+    await prisma.$queryRaw`SELECT 1`;
+    console.log("Database connected successfully.✅");
+    // GET the user count
+    const userCount = await prisma.user.count();
+    console.log(`Total users: 👥  ${userCount}`);
+  } catch (error) {
+    console.error("Failed to connect with Database.❌", error.message);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+testDatabase();

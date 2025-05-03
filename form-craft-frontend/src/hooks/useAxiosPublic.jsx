de4938020b937e19b6754bbf59520918593ba213
Sohelrana2815 <sohel152302@gmail.com>
2025-05-03 19:35:23 +0600

    In the first stage i deploy my backend server side in render.com but initial stage it takes long time to load, i fix this issue and deploy it in vercel

--- form-craft-frontend/src/hooks/useAxiosPublic.jsx
+++ form-craft-frontend/src/hooks/useAxiosPublic.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://form-craft-project.vercel.app/api",
 });
 const useAxiosPublic = () => {
   return axiosPublic;

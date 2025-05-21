fe9dcae40c261e4df75e69defb186cfba2c11b8e
Sohelrana2815 <sohel152302@gmail.com>
2025-05-21 12:59:01 +0600

    not solved the problem still getting invalid grant

--- form-craft-backend/controllers/salesforceController.js
+++ form-craft-backend/controllers/salesforceController.js
@@ -8,20 +8,24 @@ exports.createSalesforceAccount = async (req, res) => {
     // Build and send the OAuth token request
     const params = new URLSearchParams({
       grant_type: "password",
-      client_id: process.env.SALESFORCE_CONSUMER_KEY,
-      client_secret: process.env.SALESFORCE_CONSUMER_SECRET,
-      username: process.env.SALESFORCE_USERNAME,
+      client_id: process.env.SALESFORCE_CONSUMER_KEY || "",
+      client_secret: process.env.SALESFORCE_CONSUMER_SECRET || "",
+      username: process.env.SALESFORCE_USERNAME || "",
       // Make sure this var really is "password+token"
       password:
         (process.env.SALESFORCE_PASSWORD ?? "") +
         (process.env.SALESFORCE_SECURITY_TOKEN ?? ""),
     });
     console.log(params.toString());
+    console.log(
+      "Final password string:",
+      process.env.SALESFORCE_PASSWORD + process.env.SALESFORCE_SECURITY_TOKEN
+    );
 
     // // 1. Salesforce Authentication (OAuth2 password flow)
     const authResponse = await axios.post(
       "https://login.salesforce.com/services/oauth2/token",
-      params.toString(),
+      params,
       { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
     );
     console.log("Got token:", authResponse.data.access_token);
